05852e7b5c259074dff73893010aa99a94701b20
hamdain <dain9812@naver.com>
2022-06-24 04:24:11 +0900

    feat: 로딩 컴포넌트 UI 개발

---
+++ src/components/Loader.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  cursor: wait;
+`;
+
+const Spinner = styled.div`
+  border: 10px solid #f3f3f3;
+  border-top: 10px solid #3498db;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  animation: spin 1s linear infinite;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Loader = () => {
+  return (
+    <Container>
+      <Spinner />
+    </Container>
+  );
+};
+
+export default Loader;
